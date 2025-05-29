002c5bb4da4b01c1c3a18fe172e5149d2f4a91b3
Benjamin Gudehus <hastebrot@gmail.com>
2025-05-29 17:13:48 +0200

    [001] feat: Components system. (#2)

---
+++ sources/fieldset-app/src/helpers/clsx.ts
@@ -0,0 +1,11 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { clsx, type ClassValue } from "clsx";
+
+export {
+  // https://github.com/lukeed/clsx
+  clsx as classNames,
+  // https://github.com/joe-bell/cva
+  cva as classVariants,
+  type ClassValue,
+  type VariantProps,
+};
--- sources/fieldset-app/src/routes.tsx
+++ sources/fieldset-app/src/routes.tsx
@@ -1,7 +1,14 @@
 import { type RouteObject } from "react-router";
+import { SysIcon } from "./components/system/sysIcon";
 
 export const IndexScreen = () => {
-  return <div className="p-[16px]">index</div>;
+  return (
+    <div className="p-[16px] flex items-center gap-[8px]">
+      <SysIcon name="home" variant="outlined" width={20} />
+      <SysIcon name="home" variant="filled" width={20} />
+      <span>home</span>
+    </div>
+  );
 };
 
 export const routes: RouteObject[] = [
