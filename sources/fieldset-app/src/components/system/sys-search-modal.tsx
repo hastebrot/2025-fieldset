c97ea5b034a0e2f247d561423283c8ac7b78291f
Benjamin Gudehus <hastebrot@gmail.com>
2025-06-22 02:58:35 +0200

    feat: System search modal, command dialog.

--- sources/fieldset-app/src/components/system/sys-search-modal.tsx
+++ sources/fieldset-app/src/components/system/sys-search-modal.tsx
@@ -2,10 +2,54 @@
 // https://react-spectrum.adobe.com/react-aria/Autocomplete.html
 // https://react-spectrum.adobe.com/react-aria/examples/command-palette.html
 
+import { classNames } from "../../helpers/clsx";
+import { SysKeyboard } from "./sys-keyboard";
+import { SysText } from "./sys-text";
+
 export type SysSearchModalProps = {
   children?: React.ReactNode;
 };
 
 export const SysSearchModal = (props: SysSearchModalProps) => {
-  return <div>{props.children}</div>;
+  return <CommandDialog>{props.children}</CommandDialog>;
+};
+
+type CommandDialogProps = {
+  children?: React.ReactNode;
+};
+
+const CommandDialog = (props: CommandDialogProps) => {
+  return (
+    <div
+      className={classNames(
+        "flex flex-col overflow-hidden min-h-[300px]",
+        "bg-(--bg-base) shadow-(--elevation-modal) rounded-xl",
+      )}
+    >
+      <div className="flex flex-col h-full overflow-hidden">{props.children}</div>
+      <footer
+        className={classNames(
+          "flex items-center justify-end px-4 py-3",
+          "bg-(--bg-field) text-(--fg-subtle) border-t border-(--border-base)",
+        )}
+      >
+        <div className="flex items-center gap-x-3">
+          <div className="flex items-center gap-x-2">
+            <SysText size="small">Navigation</SysText>
+            <div className="flex items-center gap-x-1">
+              <SysKeyboard>↓</SysKeyboard>
+              <SysKeyboard>↑</SysKeyboard>
+            </div>
+          </div>
+          <div className="bg-(--border-strong) h-3 w-px"></div>
+          <div className="flex items-center gap-x-2">
+            <SysText size="small">Open result</SysText>
+            <div className="flex items-center gap-x-1">
+              <SysKeyboard>↵</SysKeyboard>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
--- sources/fieldset-app/src/routes/screens/week-25-screen.tsx
+++ sources/fieldset-app/src/routes/screens/week-25-screen.tsx
@@ -1,9 +1,15 @@
 import { SysFilePreview, SysFileUploadArea } from "../../components/system/sys-file-field";
+import { SysSearchModal } from "../../components/system/sys-search-modal";
 import { Story, StoryLayout } from "../layouts/story-layout";
 
 export const Week25Screen = () => {
   return (
     <StoryLayout>
+      <Story className="p-[16px] flex flex-row gap-2">
+        <div className="flex flex-col gap-2 w-[calc(500px+8px)]">
+          <SysSearchModal />
+        </div>
+      </Story>
       <Story className="p-[16px] flex flex-row gap-2">
         <div className="flex flex-col gap-2 w-[calc(500px+8px)]">
           <SysFilePreview label="filename.png" description="30.48 KB" />
